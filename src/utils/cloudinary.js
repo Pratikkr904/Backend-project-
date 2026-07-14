671d0cef30997eb56c1818a3844daca0f35195cf
Pratik Kumar <pratikkumar0719@gmail.com>
2026-07-14 21:04:21 +0530

    cloudinary updated

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"//file read write defalut in node js pack 
+
+cloudinary.config({
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadoncloudinary=async(localfilepath)=>{
+  try{
+    if(!localfilepath)return null
+     const response=await cloudinary.uploader.upload(localfilepath,{
+      resource_type:"auto"
+    })
+    console.log("File uploaded successfully",response.url);
+    return response;
+
+  }catch(error){
+    fs.unlinkSync(localfilepath)
+    return null;
+    
+  }
+}
+
+ 
