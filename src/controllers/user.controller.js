f43054595b3e57dd076efd52a332da9a9ea1ce61
Pratik Kumar <pratikkumar0719@gmail.com>
2026-07-16 21:44:31 +0530

    all setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,13 +2,13 @@ import {asynchandler} from '../utils/asynchandler.js';
 
 const registerUser = asynchandler(async (req, res) => {
   res.status(200).json({
-    message: "register ok"
+    message: "register done"
   })
 })
 
 const login = asynchandler(async (req, res) => {
   res.status(200).json({
-    message: "login ok"
+    message: "login done"
   })
 })
 
