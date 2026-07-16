9d76d51e9ae63d3d7070a5d58b2530d5e343a600
Pratik Kumar <pratikkumar0719@gmail.com>
2026-07-16 21:32:01 +0530

    error handled

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,15 @@
 import {asynchandler} from '../utils/asynchandler.js';
 
-const registerUser=asynchandler(async(requestAnimationFrame,res)=>{
-  response.status(200).json({
-    message:"ok"
+const registerUser = asynchandler(async (req, res) => {
+  res.status(200).json({
+    message: "register ok"
   })
 })
 
+const login = asynchandler(async (req, res) => {
+  res.status(200).json({
+    message: "login ok"
+  })
+})
 
-export {registerUser};
+export { registerUser, login };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-// import mongoose from "mongoose";
-// import {DB_NAME} from "./constants.js"
 import dns from 'dns';
-dns.setServers(['8.8.8.8', '1.1.1.1']);
- import connectdb from "./db/index.js";
- import dotenv from "dotenv";
+import dotenv from "dotenv";
+import connectdb from "./db/index.js";
+import app from "./app.js";
 
- dotenv.config({
-  path:'./.env'
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+dotenv.config({
+  path: './.env'
 })
+
 connectdb()
-.then(()=>{
-  app.listen(process.env.PORT||8000,()=>{
-    console.log(`server is running on port ${process.env.PORT}`)
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running on port ${process.env.PORT || 8000}`)
+    })
   })
-}).catch((err)=>{
-  console.log("Mongo db connection failed",err);
-})
+  .catch((err) => {
+    console.log("Mongo db connection failed", err);
+  })
 
 
 // import express from "express"
